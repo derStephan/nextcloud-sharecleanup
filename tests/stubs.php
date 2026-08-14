bbb050ae1530b8eb2d0d989b3d480d8a1515996e
derStephan <derStephan@users.noreply.github.com>
2026-08-14 16:28:35 +0200

    tests again

--- tests/stubs.php
+++ tests/stubs.php
@@ -193,6 +193,9 @@ namespace OCP\AppFramework\Http {
 }
 
 namespace Symfony\Component\Console\Command {
+    use Symfony\Component\Console\Input\InputInterface;
+    use Symfony\Component\Console\Output\OutputInterface;
+
     abstract class Command {
         protected ?string $name = null;
         public function __construct() {}
